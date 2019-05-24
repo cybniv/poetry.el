946b85ed333671de18987ac24d8ead953ab0162b
galaunay <gaby.launay@tutanota.com>
2019-05-24 20:33:22 +0100

    Test cover for interactive `poetry-run'

--- test/poetry-run-test.el
+++ test/poetry-run-test.el
@@ -8,4 +8,12 @@
     (with-current-buffer "*poetry*"
     (should (re-search-forward "^pyproject.toml$")))))
 
+(ert-deftest poetry-run-should-run-interactively ()
+  (let* ((ppath (poetry-test-create-project-folder))
+         (default-directory ppath))
+    (cl-letf (((symbol-function 'completing-read) (lambda (&rest ignore) "ls")))
+      (call-interactively 'poetry-run))
+    (with-current-buffer "*poetry*"
+    (should (re-search-forward "^pyproject.toml$")))))
+
 ;;; poetry-run-test.el ends here
